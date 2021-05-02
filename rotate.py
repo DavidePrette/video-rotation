f3ebb72f30dd848e49df87389c35415ff1c7a2cf
Davide Prette <davide.prette@platomics.com>
2021-05-02 22:31:14 +0200

    Final version 1.0

--- rotate.py
+++ rotate.py
@@ -1,12 +1,16 @@
 import asyncio, glob, os, ffmpeg
-
 #-vf "vflip,hflip" -r 60 -qscale 0 -acodec copy
 
-os.chdir(".")
+if not os.path.isdir('rotated'):
+    os.mkdir('rotated')
+if not os.path.isdir('input'):
+    os.mkdir('input')
+os.chdir("./input")
 for file in glob.glob("*.MP4"):
     stream = ffmpeg.input(str(file))
+    audio1 = stream.audio
     stream = ffmpeg.vflip(stream)
     stream = ffmpeg.hflip(stream)
     stream = ffmpeg.filter(stream, 'fps', fps=60, round='up')
-    stream = ffmpeg.output(stream, 'output.mp4', acodec="copy")
+    stream = ffmpeg.output(stream, audio1, str('../rotated/' + file), acodec="copy")
     ffmpeg.run(stream)
